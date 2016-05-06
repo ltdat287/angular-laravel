e4009691609699aa748494988b5aa41e3a4bbbbf
TienDat <datlt@mog.net>
2016-05-06 17:14:15 +0700

    add templates for customer and customers html

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html ng-app="app">
+    <head>
+        <title>Custom Management</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+        <script src="http://code.angularjs.org/1.2.3/angular.js"></script>
+        <script src="http://code.angularjs.org/1.2.3/angular-route.js"></script>
+        <script src="./app.js"></script>
+    </body>
+</html>
